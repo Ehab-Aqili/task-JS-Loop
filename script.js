683cef3789f5891e1519b59a77af3ad566826504
Ehab-Aqili <ihabaloqaily@gmail.com>
2023-04-29 23:50:33 +0300

    for Loop

---
+++ script.js
@@ -0,0 +1,176 @@
+// for (let i=0; i<=10; i++) {
+//      console.log(i);
+// }
+
+// console.log('***');
+
+// let z = 0;
+// while (z <= 10) {
+//     console.log(z);
+//     z++
+// }  
+// console.log('***');
+
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log('***');
+
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+// console.log('***');
+
+// for (i = 0; i <= 10; i+=2){
+//     console.log(i);
+// }
+
+// console.log('***');
+
+
+// var sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum = i + sum;
+// }
+// console.log(sum);
+
+// console.log('***');
+
+// const lag = [1, 0.2, 3, 14, 5.1];
+
+// var large = lag[0];
+
+// for (i = 0; i < lag.length; i++){
+//     if(lag[i] > large) {
+//         large = lag[i]
+//     }
+// }
+//  console.log(large);
+
+// console.log('****');
+
+// const arr = [1, 0.5, 3, 14, 5.1];
+
+// var sm = arr[0];
+
+// for (i = 0; i < arr.length; i++){
+//     if(arr[i] < sm) {
+//         sm = arr[i];
+//     }
+// }
+//  console.log(sm);
+
+
+// const arr = [1, 2, 3, 4, 5]
+
+// var sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(sum / arr.length);
+
+// var fra = 1;
+// var num = 5;
+// for (let i = 1; i <= num; i++) {
+//     fra *= i;
+// }
+// console.log(fra);
+
+// var sum = 1;
+// var num = 5;
+// for (let i = 1; i <= num; i++) {
+   
+// }
+// console.log(sum);
+
+// const num = 10;
+// let a = 0, b = 1, c;
+
+// for (let i = 0; i < num; i++) {
+//   if (i <= 1) {
+//     c = i;
+//   } else {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   console.log(c);
+// }
+
+// const num = 20;
+
+// for (let i = 2; i <= num; i++) {
+//   let Prime = true;
+
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       Prime = false;
+//       break;
+//     }
+//   }
+
+//   if (Prime) {
+//     console.log(i);
+//   }
+// }
+
+
+
+// var num = 5;
+// for (let i = 1; i <= 10; i++) {
+//     console.log( num + "*" + i + "=" + i * num);
+// }
+
+
+// const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr.length; j++) {
+//    console.log(arr[i][j])
+//   }
+// }
+
+
+// var arr = [1, 2, 3, 4, 5]
+
+// for (let i = arr.length; i > 0; i--){
+//     console.log(i)
+// }
+
+// var arr = [1, 2, 3, 4, 5];
+// var num1 = 2
+// var num2 = 4
+// for(let i = num1; i <= num2; i++){
+//     console.log(arr[i]);
+// }
+
+
+// var arr = [1, 2, 3, 4, 5];
+// var num1 = 0;
+// var towStep = 2;
+// for(let i = num1; i <= arr.length; i+=towStep){
+//     console.log(arr[i])
+// }
+
+// var arr = [1, 2, 3, 4, 5]
+// var input = 4
+// for (let i = 0; i < arr.length; i++) {
+//     if (input === arr[i]){
+//         console.log(true)
+//     } 
+// }
+
+
+
+// var arr =[1, 2, 1, 3, 2, 1]
+
+// var input = 1
+// var counter = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (input === arr[i]){
+//       counter++
+//     } 
+    
+// }
+// console.log(counter)
+
+
+
